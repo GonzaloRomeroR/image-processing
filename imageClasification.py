b7b0c9509d41a8cf1cf6d7c70005a700e151dd81
gonzalo <gonzaromero2007@gmail.com>
2018-05-06 19:56:17 -0300

    First approach finished

--- imageClasification.py
+++ imageClasification.py
@@ -1,5 +1,95 @@
+import cv2
+import numpy as np
+import os
+
+def imageProcessing(image):
+    preImage = preprocessing(image)
+    features = featureExtraction(preImage)
+    return features
+
+def preprocessing(image):
+    factorEscalaImagen = 0.25
+    preimage = cv2.resize(image, None, fx= factorEscalaImagen, fy= factorEscalaImagen,
+        interpolation= cv2.INTER_LINEAR)
+    return preimage
+
+def featureExtraction(image):
+    features = HOG(image)
+    return features
+
+def HOG(image):
+    winSize = (len(image[0]),len(image))
+    blockSize = (int(len(image[0])/4), int(len(image)/4))
+    blockStride = (int(len(image[0])/8), int(len(image)/8))
+    cellSize = (int(len(image[0])/4), int(len(image)/4))
+    nbins = 9
+    derivAperture = 1
+    winSigma = -1.
+    histogramNormType = 0
+    L2HysThreshold = 0.2
+    gammaCorrection = 1
+    nlevels = 64
+    signedGradients = True
+    hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize
+        ,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,
+            gammaCorrection,nlevels, signedGradients)
+    descriptor = hog.compute(image)
+    return descriptor
+
+def uploadDatabase(file):
+    length = len( np.fromfile(file, float, -1 , ',') )
+    matrix = np.loadtxt(file, usecols=range(length), dtype=float, delimiter=',')
+    list = matrix.tolist()
+    return (list)
+
+def KNN(k ,element, classes, vectors):
+    distances = []
+    for categories in vectors:
+        disAux = []
+        for individuals in categories:
+            sum = 0
+            for i in range(len(individuals)):
+                sum = sum + (element[i][0] - individuals[i])**2
+            disAux.append(sum)
+        distances.append(disAux)
+    scores = []
+    for i in range(len(vectors)):
+        scores.append(0)
+    while True:
+        min = 1000
+        for j in range(len(distances)):
+            for k in range(len(distances[j])):
+                if distances[j][k] < min:
+                    min = distances[j][k]
+                    rowmin = j
+                    columnmin = k
+        scores[rowmin] = scores[rowmin] + 1
+        distances[rowmin][columnmin] = 1000
+        if max(scores) == 3:
+            break;
+    print(scores)
+    category = classes[scores.index(max(scores))]
+    return category
+
 def main():
-    pass
+    classes = []
+    featuresMatrix = []
+    for subdirectory in os.listdir("data"):
+        directory = "data/" + subdirectory
+        features = uploadDatabase(directory);
+        classes.append(subdirectory)
+        featuresMatrix.append(features)
+    #imgName = input("Ingrese la imagen a clasificar:")
+    imgName = "tuerca4.jpeg"
+    image = cv2.imread(imgName, 0)
+    if image is None:
+        print("Image not found")
+    else:
+        print ("Image found")
+        procImage = imageProcessing(image).tolist()
+        k = 3
+        imageClass = KNN(k, procImage, classes, featuresMatrix)
+        print (imageClass)
 
 if __name__ == '__main__':
     main()
--- training.py
+++ training.py
@@ -3,25 +3,58 @@ import numpy as np
 import os
 
 def imageProcessing(image):
-    sift = cv2.xfeatures2d.SIFT_create()
-    kp = sift.detect(image,None)
-    img = cv2.drawKeypoints(image, kp, image)
-    cv2.imwrite('sift_keypoints.jpg', img)
-    print (kp)
-    input ('')
+    preImage = preprocessing(image)
+    features = featureExtraction(preImage)
+    return features
+
+
+def preprocessing(image):
+    factorEscalaImagen = 0.25
+    preimage = cv2.resize(image, None, fx= factorEscalaImagen, fy= factorEscalaImagen,
+        interpolation= cv2.INTER_LINEAR)
+    return preimage
+
+def featureExtraction(image):
+    features = HOG(image)
+    return features
+
+def HOG(image):
+    winSize = (len(image[0]),len(image))
+    blockSize = (int(len(image[0])/4), int(len(image)/4))
+    blockStride = (int(len(image[0])/8), int(len(image)/8))
+    cellSize = (int(len(image[0])/4), int(len(image)/4))
+    nbins = 9
+    derivAperture = 1
+    winSigma = -1.
+    histogramNormType = 0
+    L2HysThreshold = 0.2
+    gammaCorrection = 1
+    nlevels = 64
+    signedGradients = True
+    hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize
+        ,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,
+            gammaCorrection,nlevels, signedGradients)
+    descriptor = hog.compute(image)
+    return descriptor
 
 
 def train():
-    for subdirectorie in os.listdir("training"):
-        path = "training/" + subdirectorie
+    for subdirectory in os.listdir("training"):
+        exec("%s = []" % subdirectory)
+        path = "training/" + subdirectory
         for j in range(len(os.listdir(path))):
-            print (subdirectorie + str(j) + ".jpg")
-            imageName = subdirectorie + str(j) + ".jpg"
+
+            imageName = subdirectory + str(j) + ".jpeg"
             image = cv2.imread(path + "/" + imageName, 0)
             if image is None:
-                print("Image not found")
+                print("Image" + subdirectory + str(j) + ".jpeg" +" not found")
             else:
-                imageProcessing(image)
+                print (subdirectory + str(j) + ".jpeg" + " done")
+                procImage = imageProcessing(image)
+                exec("%s.append(procImage)" % subdirectory)
+        exec("npImage = np.asarray(%s)" % subdirectory)
+        exec("np.savetxt('data/%s', npImage, delimiter=',')" % subdirectory)
+
 
 def main():
     train()
